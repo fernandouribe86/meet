8ae82d1ac5a6116ce222675ccbcab11d1c96e115
Fernando A. Uribe <fauribe56@gmail.com>
2022-05-24 19:33:15 -0700

    Updated CORS

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -65,7 +65,8 @@ module.exports.getAccessToken = async (event) => {
       return {
         statusCode: 200,
         headers: {
-          "Access-Control-Allow-Origin": "*"
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
         },
         body: JSON.stringify(token)
       }})
