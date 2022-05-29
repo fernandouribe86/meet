845248b7070811cead6847d112306b4b63a2e80d
Fernando A. Uribe <96088622+fernandouribe86@users.noreply.github.com>
2022-05-29 16:33:05 -0700

    Updated chart width

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ class App extends Component {
             </div>
             <div id="right-column">
               <ScatterChart
-                width={400}
+                width={800}
                 height={400}
                 margin={{
                   top: 20, right: 20, bottom: 20, left: 20,
