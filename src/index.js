180725ddcab00df553eac5ba283ac9db7927befd
Fernando A. Uribe <96088622+fernandouribe86@users.noreply.github.com>
2022-05-29 11:05:46 -0700

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './service-worker';
-// import * as serviceWorkerRegistration from '../src/serviceWorkerRegistration';
+// import * as serviceWorker from './service-worker';
+import * as serviceWorkerRegistration from '../src/serviceWorkerRegistration';
 
 import * as atatus from 'atatus-spa';
 atatus.config('d3c849e2033840ffa951e34cdd7d1319').install();
@@ -16,7 +16,7 @@ root.render(
   </React.StrictMode>
 );
 
-serviceWorker.register();
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
