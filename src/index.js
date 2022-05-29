4aa8b1f3983b5aaaf2e2524c087f8497ae22356e
Fernando A. Uribe <96088622+fernandouribe86@users.noreply.github.com>
2022-05-29 11:30:22 -0700

    Update Service Workers and Index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from '../src/serviceWorkerRegistration';
-import * as serviceWorker from '../src/service-worker';
+// import * as serviceWorker from '../src/service-worker';
 
 import * as atatus from 'atatus-spa';
 atatus.config('d3c849e2033840ffa951e34cdd7d1319').install();
@@ -16,7 +16,7 @@ root.render(
   </React.StrictMode>
 );
 
-serviceWorker.register();
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
