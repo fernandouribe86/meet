d50ecae1397c100699730c4329726d093d706403
Fernando A. Uribe <96088622+fernandouribe86@users.noreply.github.com>
2022-05-29 10:53:00 -0700

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from '../src/serviceWorker';
+import * as serviceWorker from '../src/service-worker';
 import * as serviceWorkerRegistration from '../src/serviceWorkerRegistration';
 import * as atatus from 'atatus-js';
 
