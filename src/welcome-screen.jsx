eb20953300def039a266f40e68c337eed7f90ec6
Fernando A. Uribe <96088622+fernandouribe86@users.noreply.github.com>
2022-06-04 11:09:33 -0700

    Testing Welcome Screen

---
+++ src/welcome-screen.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function WelcomeScreen(props) {
+  return props.showWelcomeScreen ?
+    (
+      <div>
+        <h1>Welcome to meet!</h1>
+        <h4>
+          Log in to see upcoming events around the world for
+          full-stack
+          developers
+        </h4>
+        <div className="button_cont" align="center">
+          <div >
+            <button onClick={() => { props.getAccessToken() }}
+              rel="nofollow noopener"
+            >
+              <b>Sign in with google</b>
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+    : null
+}
+
+export default WelcomeScreen;
